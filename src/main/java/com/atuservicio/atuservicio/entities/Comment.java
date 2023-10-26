4025811fd523d1ada90b7f7dfd3b99fa7749bb7a
JoelRondinelPacheco <joel.rondinel.pacheco@gmail.com>
2023-10-26 09:28:35 -0300

    Entidades arregladas

--- src/main/java/com/atuservicio/atuservicio/entities/Comment.java
+++ src/main/java/com/atuservicio/atuservicio/entities/Comment.java
@@ -1,16 +1,16 @@
 package com.atuservicio.atuservicio.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@Table(name = "comments")
 public class Comment extends Base {
     @ManyToOne
     @JoinColumn(name = "autor_id")
     private User emisor;
+    @ManyToOne
+    @JoinColumn(name = "receptor_id")
     private User receptor;
     private Date date;
     private Double puntaje;
--- src/main/java/com/atuservicio/atuservicio/entities/User.java
+++ src/main/java/com/atuservicio/atuservicio/entities/User.java
@@ -2,19 +2,22 @@ package com.atuservicio.atuservicio.entities;
 
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.management.relation.Role;
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@Table(name = "users")
 public class User extends Base {
     private String name;
     @Column(unique = true)
     private String email;
     private String password;
     Boolean active;
-    @ManyToOne
+    @ManyToMany
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private List<Role> role;
     @OneToMany(mappedBy = "emisor", cascade = CascadeType.ALL)
     private List<Comment> comments;
+    @OneToMany(mappedBy = "receptor", cascade = CascadeType.ALL)
+    private List<Comment> comentarios_recibidos;
 }
