2b855652686aa18ba6357f708db7b043aee56df1
JoelRondinelPacheco <joel.rondinel.pacheco@gmail.com>
2023-10-27 20:59:11 -0300

    Capa de servicios

--- src/main/java/com/atuservicio/atuservicio/services/UserService.java
+++ src/main/java/com/atuservicio/atuservicio/services/UserService.java
@@ -2,13 +2,27 @@ package com.atuservicio.atuservicio.services;
 
 import com.atuservicio.atuservicio.dtos.SaveUserDTO;
 import com.atuservicio.atuservicio.dtos.UserInfoDTO;
+import com.atuservicio.atuservicio.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class UserService implements IUserService{
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserInfoDTO save(SaveUserDTO user) {
-        return null;
+        User userfinal = new User();
+        userfinal.setName(user.getName());
+        userfinal.setEmail(user.getEmail());
+        //...
+        User userGuardado = this.userRepository.save(userfinal);
+        UserInfoDTO userinfo = new UserInfoDTO(userGuardado.getId(), userGuardado.getName(), );
+
+
+        return userinfo;
     }
 
     @Override
@@ -30,4 +44,5 @@ public class UserService implements IUserService{
     public List<UserInfoDTO> getAllUsers() {
         return null;
     }
+
 }
