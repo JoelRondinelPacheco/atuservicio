7e7adf3174b86cf96e51def9085772f047838a6f
javote94 <127157520+javote94@users.noreply.github.com>
2023-10-28 15:46:07 -0300

    Entidades y repositorios

--- src/main/java/com/atuservicio/atuservicio/repositories/UserRepository.java
+++ src/main/java/com/atuservicio/atuservicio/repositories/UserRepository.java
@@ -4,9 +4,13 @@ package com.atuservicio.atuservicio.repositories;
 
 import com.atuservicio.atuservicio.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String>{
-
+    
+    @Query("SELECT u FROM users u WHERE u.email = :email")
+    public User findByEmail(@Param("email")String email);
 }
