0f7e4174ebe82ed86b127bd4d95206b60c917506
hernadar <dario.hernandez@gmail.com>
2023-10-28 16:50:22 -0300

    cambios controladores

--- src/main/java/com/atuservicio/atuservicio/controllers/UserController.java
+++ src/main/java/com/atuservicio/atuservicio/controllers/UserController.java
@@ -10,6 +10,7 @@ package com.atuservicio.atuservicio.controllers;
 import com.atuservicio.atuservicio.dtos.SaveUserDTO;
 import com.atuservicio.atuservicio.dtos.UserInfoDTO;
 import com.atuservicio.atuservicio.entities.Role;
+import com.atuservicio.atuservicio.entities.User;
 import com.atuservicio.atuservicio.exceptions.MyException;
 import com.atuservicio.atuservicio.services.UserService;
 
@@ -112,5 +113,14 @@ public class UserController {
 
         return "index.html";
     }
-
+    @GetMapping("/list")
+    public String list(ModelMap model){
+        
+        List<UserInfoDTO> users = userService.getAllUsers();
+        
+        model.addAttribute("users", users);
+       
+        return "users_list.html";
+        
+    }
 }
