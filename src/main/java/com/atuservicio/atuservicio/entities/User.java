3cabab6656184e186d73f8cfe6ec9a367984d9f1
JoelRondinelPacheco <joel.rondinel.pacheco@gmail.com>
2023-11-01 18:45:46 -0300

    Tarea 1, 2, 6 y 7 completas

--- src/main/java/com/atuservicio/atuservicio/entities/User.java
+++ src/main/java/com/atuservicio/atuservicio/entities/User.java
@@ -80,22 +80,22 @@ public class User extends Base implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return this.active;
     }
 
 
