7c039b59fa25629b82889bf852e637f000cca1d7
Juan <juanverdini.97@gmail.com>
2023-11-01 21:32:02 -0300

    sdsdd

--- src/main/java/com/atuservicio/atuservicio/services/CategoryService.java
+++ src/main/java/com/atuservicio/atuservicio/services/CategoryService.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.atuservicio.atuservicio.services;
 
-/**
- *
- * @author PC
- */
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class CategoryService {
     
 }
