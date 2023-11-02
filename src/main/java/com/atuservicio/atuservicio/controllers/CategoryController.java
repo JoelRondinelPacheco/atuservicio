a6940715b5ae5effc840e80d0b25f72b7af2b33e
Juan <juanverdini.97@gmail.com>
2023-11-02 19:35:15 -0300

    actualizo

--- src/main/java/com/atuservicio/atuservicio/controllers/CategoryController.java
+++ src/main/java/com/atuservicio/atuservicio/controllers/CategoryController.java
@@ -6,6 +6,8 @@
 package com.atuservicio.atuservicio.controllers;
 
 import com.atuservicio.atuservicio.exceptions.MyException;
+import com.atuservicio.atuservicio.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/atuservicio/atuservicio/services/CategoryService.java
+++ src/main/java/com/atuservicio/atuservicio/services/CategoryService.java
@@ -1,9 +1,14 @@
 package com.atuservicio.atuservicio.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class CategoryService {
+    @Autowired
+    CategoryRepositoy categoryRepository;
+    
+    
     
 }
